adfe20fe5b324ff3c92d48a619ed3c19502aef6b
Githmi Niseka <108750891+GithmiNiseka@users.noreply.github.com>
2024-03-01 11:07:34 +0530

    Update Awrapper.jsx

--- src/components/about/Awrapper.jsx
+++ src/components/about/Awrapper.jsx
@@ -8,11 +8,11 @@ const Awrapper = () => {
         <div className='container grid'>
           {awrapper.map((val) => {
             return (
-              <div className='box flex'>
-                <div className='img'>
+              <div className='box flex' style={{position:'relative',top:'-20px',left:'100px'}}>
+                <div className='img' style={{ml:'15px',}}>
                   <img src={val.cover} alt='' />
                 </div>
-                <div className='text'>
+                <div className='text' style={{ml:'15px',mr:'-5px'}}>
                   <h1>{val.data}</h1>
                   <h3>{val.title}</h3>
                 </div>
